971f850e625d5fe3a597accc561dabdae9efc4ec
Arun Kumar M S <arun.einstein@gmail.com>
2014-09-21 21:26:58 -0700

    some more customizations

--- init.el
+++ init.el
@@ -4,12 +4,13 @@
       "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
-(set-default-font "Inconsolata-13")
-
-(require 'solarized)
-(require 'solarized-dark-theme)
+(set-default-font "Source Code Pro-12")
+(load-theme 'solarized-dark)
+(windmove-default-keybindings)
 (require 'enclose)
 (enclose-mode t)
+
+(electric-pair-mode)
 
 (server-start)
 
@@ -23,7 +24,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
+ '(custom-safe-themes (quote ("e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
